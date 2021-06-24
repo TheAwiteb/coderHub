a820aafe6b1974dcdb426f0dedd3b5a88dae894e
Awiteb <59842932+Awiteb@users.noreply.github.com>
2021-06-24 10:35:08 +0300

    first commit

--- coderHubSa/main.py
+++ coderHubSa/main.py
@@ -2,3 +2,9 @@
 This will be the file that is the main file, to work on the coderHubSa project, 
 the project will be a library that helps you access the contents of the coderHub.sa site
 """
+
+import requests
+
+class CoderHub():
+    def __init__(self):
+        pass
