2b1834af71b335c1e885e05301eff66c54c3bde1
Awiteb <59842932+Awiteb@users.noreply.github.com>
2021-06-24 18:36:47 +0300

    add: get_challengs

--- coderHubSa/main.py
+++ coderHubSa/main.py
@@ -4,11 +4,36 @@ the project will be a library that helps you access the contents of the coderHub
 """
 
 import requests
-import typing
+from typing import Optional, Union
 
-get_challeng_url = "https://api.coderhub.sa/api/challenges/detail/{}"
-challenges_url = "https://api.coderhub.sa/api/challenges/filtered-list/"
+get_challenge_url = "https://api.coderhub.sa/api/challenges/detail/{}"
+challenges_url = "https://api.coderhub.sa/api/challenges/filtered-list/?page_size=9999999999"
 
 class CoderHub():
     def __init__(self):
         pass
+    def get_challengs(self, difficulty: Optional[Union[str, None]] = None):
+        """ Returns all challenges by difficulty, if difficulty not None, else he will return all challenges
+        difficulty should be in ['easy', 'normal', 'hard']
+
+        Args:
+            difficulty (Optional[Union[str, None]], optional): difficulty of challenges. Defaults to None.
+        
+        Raises:
+            Exception: Invalid difficulty
+        
+        Returns:
+            dict: dictionary of challenges
+        """
+        challenges = requests.get(challenges_url).json()
+        difficulty_list = ['easy', 'normal', 'hard']
+        if difficulty:
+            if difficulty in difficulty_list:
+                difficulty_id = difficulty_list.index(difficulty) + 2 # coderHub count start from 2
+                return {'result':list(filter(
+                    lambda challenge: challenge['type_of_level']['id'] == difficulty_id,
+                        challenges['result']))}
+            else:
+                raise Exception(f"difficulty must be {' or '.join(difficulty_list)}")
+        else:
+            return challenges['result']
