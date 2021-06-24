7b95a32df2b675adae79443efebae8ce8d845e29
Awiteb <59842932+Awiteb@users.noreply.github.com>
2021-06-24 11:26:26 +0300

    add urls

--- coderHubSa/main.py
+++ coderHubSa/main.py
@@ -4,6 +4,10 @@ the project will be a library that helps you access the contents of the coderHub
 """
 
 import requests
+import typing
+
+get_challeng_url = "https://api.coderhub.sa/api/challenges/detail/{}"
+challenges_url = "https://api.coderhub.sa/api/challenges/filtered-list/"
 
 class CoderHub():
     def __init__(self):
