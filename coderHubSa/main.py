c2866465c4eb0d4541e7c7b45a0564486c172934
jo <wahajyousef19@gmail.com>
2021-06-24 12:34:48 +0300

    add:get_challenge_by_id

--- coderHubSa/main.py
+++ coderHubSa/main.py
@@ -1,14 +1,29 @@
-"""
-This will be the file that is the main file, to work on the coderHubSa project, 
-the project will be a library that helps you access the contents of the coderHub.sa site
-"""
 
 import requests
 import typing
 
-get_challeng_url = "https://api.coderhub.sa/api/challenges/detail/{}"
+get_challenge_url = "https://api.coderhub.sa/api/challenges/detail/{}"
 challenges_url = "https://api.coderhub.sa/api/challenges/filtered-list/"
+
 
 class CoderHub():
     def __init__(self):
         pass
+
+    def get_challenge_by_id(self, id: int):
+        """ Returns challenge object by id
+
+        Args:
+            id (int): id of challenge
+
+        Raises:
+            Exception: Invalid challenge id
+
+        Returns:
+            dict: object of challenge
+        """
+        request = requests.get(get_challenge_url.format(str(id)))
+        if request.status_code == 200:
+            return request.json()
+        else:
+            raise Exception("Invalid challenge id: challenge id not found")
