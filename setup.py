1f3ea103dd26b3b35ad57dc52ce8cb6cae2c7b23
Awiteb <59842932+Awiteb@users.noreply.github.com>
2021-06-27 09:35:25 +0300

    add setup.py

---
+++ setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+VERSION = "1.0.0"
+README_FILENAME = "README.md"
+KEYWORD = ['coderHub', 'programming challenges', 'challenges',]
+
+with open(README_FILENAME, "r", encoding="utf-8") as readme_file:
+    long_description = readme_file.read()
+with open("requirements.txt", encoding="utf-8") as require_file:
+    requires = [require.strip() for require in require_file]
+
+setuptools.setup(
+    name="coderHub",
+    version=VERSION,
+    author="Awiteb",
+    author_email="Awiteb@hotmail.com",
+    description="A python method based on the API of the coderHub.sa, which helps you to fetch the challenges and more",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Awiteb/coderHub",
+    packages=setuptools.find_packages(),
+    keywords=KEYWORD,
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+    install_requires=requires,
+)
